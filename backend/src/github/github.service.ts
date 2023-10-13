70894e6d2b7c53d7df31faf69aade698a47b7653
ricardo049025 <86623237+ricardo049025@users.noreply.github.com>
2023-10-13 15:51:08 -0600

    feature: calling github api and create unit test (#2)
    Co-authored-by: Ricardo Martinez <Ricardo.Martinez@ext.abcdin.cl>

---
+++ backend/src/github/github.service.ts
@@ -0,0 +1,28 @@
+// src/github/github.service.ts
+
+import { Injectable } from '@nestjs/common';
+import { Octokit } from '@octokit/rest';
+
+@Injectable()
+export class GithubService {
+  private readonly octokit: Octokit;
+
+  private readonly owner: string = process.env.GITHUB_OWNER;
+  private readonly repo: string = process.env.GITHUB_REPO;
+
+  constructor() {
+    this.octokit = new Octokit({ auth: process.env.TOKEN });
+  }
+
+  async getCommits(): Promise<any[]> {
+    try {
+      const response = await this.octokit.repos.listCommits({
+        owner: this.owner,
+        repo: this.repo,
+      });
+      return response.data;
+    } catch (error) {
+      throw new Error(`Failed to fetch commits: ${error.message}`);
+    }
+  }
+}
