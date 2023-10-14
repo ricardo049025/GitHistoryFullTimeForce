7c87d3077c2371febe67f9298cc1cb6545b662ba
ricardo049025 <86623237+ricardo049025@users.noreply.github.com>
2023-10-14 07:29:16 -0600

    feature: add try-catch an edit env file (#3)
    Co-authored-by: Ricardo Martinez <Ricardo.Martinez@ext.abcdin.cl>

--- backend/src/test/github.controller.spec.ts
+++ backend/src/test/github.controller.spec.ts
@@ -1,23 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GithubController } from '../github/github.controller';
 import { GithubService } from '../github/github.service';
-import { ConfigModule } from '@nestjs/config';
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+jest.mock('../github/github.service');
 
 describe('GithubController', () => {
-  let gitHubController: GithubController;
+  let githubController: GithubController;
+  let githubService: GithubService;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       controllers: [GithubController],
       providers: [GithubService],
-      imports: [ConfigModule.forRoot()],
     }).compile();
-    gitHubController = app.get<GithubController>(GithubController);
+
+    githubController = module.get<GithubController>(GithubController);
+    githubService = module.get<GithubService>(GithubService);
   });
 
-  describe('GitHubController', () => {
-    it('should call the controller getCommits', () => {
-      expect(gitHubController.getCommits()).toBeDefined();
+  describe('getCommits', () => {
+    it('should call getCommits from GithubService and return its result', async () => {
+      const mockCommits = ['commit1', 'commit2'];
+      jest.spyOn(githubService, 'getCommits').mockResolvedValue(mockCommits);
+
+      const result = await githubController.getCommits();
+
+      expect(result).toBe(mockCommits);
+      expect(githubService.getCommits).toHaveBeenCalled();
+    });
+
+    it('should throw HttpException if GithubService throws an error', async () => {
+      const errorMessage = 'Error fetching commits';
+      const errorStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+      jest.spyOn(githubService, 'getCommits').mockRejectedValue({
+        message: errorMessage,
+        status: errorStatus,
+      });
+
+      await expect(githubController.getCommits()).rejects.toThrowError(
+        new HttpException(errorMessage, errorStatus),
+      );
     });
   });
 });
